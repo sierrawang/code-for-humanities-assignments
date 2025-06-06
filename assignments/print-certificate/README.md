1716af6a9d3bfd2ef665f6e106c81e8fb4e960e6
TJ Jefferson <tjefferson4040@gmail.com>
2025-06-06 16:18:45 -0700

    fix

---
+++ assignments/print-certificate/README.md
@@ -0,0 +1,7 @@
+# Print Certificate
+In this challenge, you will use functions to print an certificate in the console.
+
+You can see how the certificate should look in `example.png`. Not that each line of the certificate should be printed using a function call, but that you don't need one unique function for each line.
+
+The width of the certificate is 10 characters (NOT including the "|" that act as borders). The hyphenated lines contain 9 and 8 hyphens respectively.
+
--- assignments/print-certificate/solution.py
+++ assignments/print-certificate/solution.py
@@ -1,48 +1,57 @@
+import random
 
-
-def top():
+def top(line_width=10):
     top_str = " "
-    for i in range(10):
+    for i in range(line_width):
         top_str += "_"
     top_str += " "
     print(top_str)
 
-def code_for_humanities():
-    print("|Code 4 Hum|")
+def code_for_humanities(line_width=10):
+    code_str = "|"
+    code_str += "C4H"
+    for i in range(line_width - 3):
+        code_str += " "
+    code_str += "|"
+    print(code_str)
 
-def middle():
+def middle(line_width=10):
     middle_str = "|"
-    for i in range(10):
+    for i in range(line_width):
         middle_str += " "
     middle_str += "|"
     print(middle_str)
 
 
-def filler(line_width):
+def filler(dash_length, line_width=10):
     filler_str = "|"
-    for i in range(line_width):
+    for i in range(dash_length):
         filler_str += "-"
-    for i in range(10 - line_width):
+    for i in range(line_width - dash_length):
         filler_str += " "
     filler_str += "|"
     print(filler_str)
 
 
-def bottom():
+def bottom(line_width=10):
     top_str = "|"
-    for i in range(10):
+    for i in range(line_width):
         top_str += "_"
     top_str += "|"
     print(top_str)
 
 def main():
     # write your code here
-    top()
-    code_for_humanities()
-    for i in range(6):
-        filler(i)
-    middle()
-    bottom()
+    line_width = 10
+    top(line_width)
+    code_for_humanities(line_width)
+    filler(8, line_width)
+    filler(9, line_width)
+    filler(8, line_width)
+    filler(9, line_width)
+    filler(8, line_width)
+    middle(line_width)
+    bottom(line_width)
 
 if __name__ == "__main__":
     main()

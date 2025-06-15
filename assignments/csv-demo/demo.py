e2b3fbcd5f064ff774aac17285052b7ca1ca094b
Sierra Wang <sierrawang97@gmail.com>
2025-06-15 16:16:55 -0700

    demo stuff

--- assignments/csv-demo/demo.py
+++ assignments/csv-demo/demo.py
@@ -5,8 +5,42 @@ def main():
     df = pd.read_csv('professors.csv')
 
     # Print the number of rows in the df
-    print(f"There are {len(df)} professors in this class!")
-	
+    # print(f"There are {len(df)} professors in this class!")
+
+    # sorted_profs = df.sort_values(by=['hair_length', 'name'], ascending=True)
+    # for index,row in sorted_profs.iterrows():
+    #     print(f'{row["hair_length"]}, {row["name"]}')
+
+    no_history_or_religion = df[(df['department'] != 'History') & (df['department'] != 'Religious Studies')]
+    # notna_series = no_history_or_religion['name'].notna()
+    # print(notna_series)
+
+    # no_missing = no_history_or_religion[notna_series]
+    print(no_history_or_religion)
+
+    # Get a series reflecting who the history professors are
+    # is_history_series = df["department"] == "History"
+    # print(is_history_series)
+
+    # # Get the department column
+    # department_column = df["department"]
+    # print(department_column)
+    # print("****************")
+
+    # # Get a dataframe of the history professors
+    # history_profs_df = df[df["department"] == "History"]
+    # print(history_profs_df)
+
+    # Loop over every row in the dataframe and print out the values
+    # for bob in df.iterrows():
+    #     bob_index = bob[0]
+    #     bob_row = bob[1]
+        
+    #     print(bob)
+    #     # print(f"{bob_index}: {bob_row}")
+    #     print('~~~~~~~~~')
+    #     print()
+    #     # print(f"Professor {bob_row['name']} studies {bob_row['department']} and has {bob_row['hair_length']} hair!")
 
 
 if __name__ == '__main__':
---
+++ assignments/pandas-demo/demo.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import csv
+
+NUM_STUDENTS = 9915
+NUM_FACULTY = 2345
+
+def main():
+    # Load the data
+    df = pd.read_csv("stanford.csv")
+
+    # Convert the values in the faculty column to be percentages
+
+
+    # Calculate the student to faculty ratio in each department
+    # (handle any missing values as zero)
+
+if __name__ == "__main__":
+    main()
